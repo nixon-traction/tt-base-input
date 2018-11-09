19444a7fe05f099122dd901163e846ceafdcdd05
Nixon <nixon@mytractiontools.com>
2018-11-08 20:30:08 -0800

    working propagation of data

--- src/App.js
+++ src/App.js
@@ -9,11 +9,30 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data : 56
+      data : 56,
+      person : {
+        name : 'bellson bob',
+        age : 15
+      }
     }
   }
   IsValid() {
     return false;
+  }
+
+  changeName(e) {
+
+    let person = Object.assign({}, this.state.person);
+    person.name = e.target.value;
+
+    this.setState({ person });
+  }
+
+  changeToAnotherName() {
+    let person = Object.assign({}, this.state.person);
+    person.name = 'Hey';
+
+    this.setState({ person });
   }
 
   render() {
@@ -23,9 +42,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <Hoc text={'hello'}/>
-        <InputComponent validationRegex={/c/ig} model={this.state.data} />
-
+        {/* <Hoc text={'hello'}/>
+        <InputComponent validationRegex={/c/ig} model={this.state.data} /> */}
+        <button onClick={()=> this.changeToAnotherName()} />
+        <PersonInput person={this.state.person} changeName={this.changeName.bind(this)} />
+        <PersonInput person={this.state.person} />
         </header>
       </div>
     );
@@ -33,3 +54,26 @@ class App extends Component {
 }
 
 export default App;
+
+
+class PersonInput extends Component {
+
+  // constructor(props) {
+  //   super(props);
+
+  //   debugger;
+  // }
+
+  render() {
+    return (
+      <>
+        <label>Name</label>
+        <input type="text" value={this.props.person.name} onChange={(e)=> this.props.changeName(e)}/>
+
+        <label>age</label>
+        <input type="text" value={this.props.person.age}/>
+      </>
+    );
+  }
+
+}
